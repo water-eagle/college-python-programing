f7ee5e9bc51fa33137fd21f9648f47ea4d33fdce
water-eagle <130212192+water-eagle@users.noreply.github.com>
2025-11-05 00:07:15 +0900

    week6 폴더의 코드 자동 정렬

--- midterm_exam_range/week6/week6_ch6_ex9.py
+++ midterm_exam_range/week6/week6_ch6_ex9.py
@@ -1,19 +1,50 @@
-# 6장 연습문제 9번
-# 사용자가 알파벳 소문자 3글자와 숫자 2글자로 패스워드를 만들었다고 하자. 이 패스워드를 알아 맞추는 프로그램을 작성해보자. 본문의 Lab을 참고한다.
+"""
+6장 연습문제 9번
+사용자가 알파벳 소문자 3글자와 숫자 2글자로 패스워드를 만들었다고 하자.
+이 패스워드를 알아 맞추는 프로그램을 작성해보자.
+본문의 Lab을 참고한다.
+"""
 
 import sys
 
 user_pass = input("패스워드를 입력하시오: ")
-pwd_str = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-pwd_num = ['0', '1', '2', '3', '4' ,'5', '6', '7', '8', '9']
+pwd_str = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+pwd_num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 for letter1 in pwd_str:
     for letter2 in pwd_str:
         for letter3 in pwd_str:
             for letter4 in pwd_num:
                 for letter5 in pwd_num:
-                    guess = letter1+letter2+letter3+letter4+letter5
-                    #print(guess)
+                    guess = letter1 + letter2 + letter3 + letter4 + letter5
+                    # print(guess)
                     if guess == user_pass:
-                        print("당신의 패스워드는 "+guess)
+                        print("당신의 패스워드는 " + guess)
                         sys.exit()
--- midterm_exam_range/week6/week6_ch6_spiral2.py
+++ midterm_exam_range/week6/week6_ch6_spiral2.py
@@ -1,25 +1,25 @@
 import turtle as t
 
-# 색상은 리스트에 저장했다가 하나씩 꺼내서 변경하도록 하자. 
-colors = ["red", "purple", "blue", "green", "yellow", "orange" ]
+# 색상은 리스트에 저장했다가 하나씩 꺼내서 변경하도록 하자.
+colors = ["red", "purple", "blue", "green", "yellow", "orange"]
 
-# 배경색은 다음과 같은 문장으로 변경이 가능하다. 
+# 배경색은 다음과 같은 문장으로 변경이 가능하다.
 t.bgcolor("black")
 
-# 거북이의 속도는 0으로 설정하면 최대가 된다. 
+# 거북이의 속도는 0으로 설정하면 최대가 된다.
 t.speed(0)
 
-# 거북이가 그리는 선의 두께는 width()를 호출하면 된다. 
+# 거북이가 그리는 선의 두께는 width()를 호출하면 된다.
 t.width(3)
 
-length = 10 # 초기 선의 길이는 10으로 한다. 
+length = 10  # 초기 선의 길이는 10으로 한다.
 
-# while 반복문이다. 선의 길이가 500보다 작으면 반복한다.  
+# while 반복문이다. 선의 길이가 500보다 작으면 반복한다.
 
-while length < 500: 
-    t.forward(length)           # length만큼 전진한다. 
-    t.pencolor(colors[length%6])        # 선의 색상을 변경한다. 
-    t.right (89)                # 89도 오른쪽으로 회전한다. 
-    length += 5         # 선의 길이를 5만큼 증가한다. 
+while length < 500:
+    t.forward(length)  # length만큼 전진한다.
+    t.pencolor(colors[length % 6])  # 선의 색상을 변경한다.
+    t.right(89)  # 89도 오른쪽으로 회전한다.
+    length += 5  # 선의 길이를 5만큼 증가한다.
 
-t.done() # 거북이 그래픽 창을 닫지 않고 유지한다.
+t.done()  # 거북이 그래픽 창을 닫지 않고 유지한다.

120024fb57bc9f7e5da571f3f71130a6b4e4880b
water-eagle <130212192+water-eagle@users.noreply.github.com>
2025-11-04 23:54:23 +0900

    week4 폴더의 3장연습문제11번(수정본) 코드 자동 정렬

--- midterm_exam_range/week4/3장연습문제11번(수정본).py
+++ midterm_exam_range/week4/3장연습문제11번(수정본).py
@@ -5,15 +5,15 @@
 # 2024136021 장을수
 import time
 
-#1970년이후 불러온 초가 실수로 반횐됨
+# 1970년이후 불러온 초가 실수로 반횐됨
 fesconds = time.time()
 total_sec = int(fesconds)
 
-#흘러온 초를 분으로 바꾸고, 현재 몇분인지 계산
+# 흘러온 초를 분으로 바꾸고, 현재 몇분인지 계산
 total_min = total_sec // 60
 minute = total_min % 60
 
-#흘러온 분을 시간으로 바꾸고, 현재 몇시인지계산
+# 흘러온 분을 시간으로 바꾸고, 현재 몇시인지계산
 total_hour = total_min // 60
 hour = total_hour % 24
 

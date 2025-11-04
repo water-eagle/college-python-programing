8ca6ac94a0e56f77b186206e5d664f8031e721ed
water-eagle <130212192+water-eagle@users.noreply.github.com>
2025-11-04 23:37:59 +0900

    week3 폴더의 코드 자동 정렬

--- midterm_exam_range/week3/week3_car1_review.py
+++ midterm_exam_range/week3/week3_car1_review.py
@@ -1,6 +1,7 @@
 import turtle as t
+
 t.shape("turtle")
-t.speed(10) # 속도
+t.speed(10)  # 속도
 t.pendown()
 
 # 자동차 크기 입력 받기
@@ -12,39 +13,39 @@ size = int(input("원하시는 자동차 크기를 입력하세요: "))
 # 큰 자동차 몸체 200*100 8*4
 t.fillcolor("red")
 t.begin_fill()
-t.forward(size*8)
+t.forward(size * 8)
 
 t.left(90)
-t.forward(size*4)
+t.forward(size * 4)
 
 t.left(90)
-t.forward(size*8)
+t.forward(size * 8)
 
 t.left(90)
-t.forward(size*4)
+t.forward(size * 4)
 
 t.left(90)
 t.end_fill()
 # 창문 그리기 이동 200*100 4*2
-t.forward(size*8)
+t.forward(size * 8)
 
 t.left(90)
-t.forward(size*4)
+t.forward(size * 4)
 
 t.left(90)
 # 그리기 100*50
 t.fillcolor("yellow")
 t.begin_fill()
-t.forward(size*2)
+t.forward(size * 2)
 t.right(90)
 
-t.forward(size*2)
+t.forward(size * 2)
 t.left(90)
 
-t.forward(size*4)
+t.forward(size * 4)
 t.left(90)
 
-t.forward(size*2)
+t.forward(size * 2)
 t.right(90)
 t.end_fill()
 
@@ -61,7 +62,7 @@ t.circle(size)
 t.end_fill()
 
 t.left(90)
-t.forward(size*6)
+t.forward(size * 6)
 
 t.right(90)
 t.fillcolor("black")
--- midterm_exam_range/week3/week3_car_new.py
+++ midterm_exam_range/week3/week3_car_new.py
@@ -1,11 +1,12 @@
 import turtle as t
+
 t.shape("turtle")
-t.speed(10) # 속도
+t.speed(10)  # 속도
 t.pendown()
 
 # 자동차 크기 입력 받기
 body_dx = int(input("자동차 몸체 x축 길이: "))
-body_dy = int(input("자동차 몸체 y축 길이: " ))
+body_dy = int(input("자동차 몸체 y축 길이: "))
 window_dx = int(input("자동차 창문 x축 길이: "))
 window_dy = int(input("자동차 창문 y축 길이: "))
 wheel_radius = int(input("자동차 바퀴 반지름: "))
@@ -18,8 +19,8 @@ wheel_color = input("자동차 바퀴 색깔: ")
 
 # 자동차가 창 중심에 있도록 펜촉을 이동한다.
 t.up()
-t.goto(-body_dx/2, -body_dy/2) # 펜촉을 왼쪽 아래 꼭지점으로 이동
-t.down() # 펜을 내린다.
+t.goto(-body_dx / 2, -body_dy / 2)  # 펜촉을 왼쪽 아래 꼭지점으로 이동
+t.down()  # 펜을 내린다.
 
 # 자동차 몸체 색상을 body_color로 지정하고, 채우기 시작하고, 몸체를 그린다.
 t.fillcolor(body_color)
@@ -37,8 +38,8 @@ t.left(90)
 
 # 자동차 상체그리기 위해 좌표이동
 t.up()
-t.goto(-window_dx/2, body_dy/2) # 펜촉 왼쪽 아래 꼭지점으로 이동
-t.down() # 펜을 내린다.
+t.goto(-window_dx / 2, body_dy / 2)  # 펜촉 왼쪽 아래 꼭지점으로 이동
+t.down()  # 펜을 내린다.
 
 # 자동차 상체 색상을 window_color로 지정하고, 채우기 시작하고, 몸체를 그린다.
 t.fillcolor(body_color)
@@ -57,16 +58,20 @@ t.end_fill()
 
 # 타이어색상을 wheel_color로 지정하고, 채우기 시작하고, 타이어를 그린다.
 t.up()
-t.goto(-(body_dx/4/wheel_radius), -body_dy/2) # 펜촉을 왼쪽 타이어 시작점으로 이동
+t.goto(
+    -(body_dx / 4 / wheel_radius), -body_dy / 2
+)  # 펜촉을 왼쪽 타이어 시작점으로 이동
 t.left(90)
 t.down()
 
 t.fillcolor(wheel_color)
 t.begin_fill()
-t.circle(wheel_radius) # 원을 그린다.and
+t.circle(wheel_radius)  # 원을 그린다.and
 
 t.up()
-t.goto(body_dx/4-wheel_radius, -body_dy/2) # 펜 촉을 오른쪽 타이어 시작점으로 이동
+t.goto(
+    body_dx / 4 - wheel_radius, -body_dy / 2
+)  # 펜 촉을 오른쪽 타이어 시작점으로 이동
 t.down()
 
 t.circle(wheel_radius)

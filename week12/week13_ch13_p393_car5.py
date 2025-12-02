a1f731d0492e54c404aa849379b50a56185f20d4
water-eagle <130212192+water-eagle@users.noreply.github.com>
2025-12-03 00:52:59 +0900

    p393 - 10개 객체를 생성하려면? 리스트에 Car 객체를 10개 생성하여 저장하고, 각 자동차가 무작위로 전진하고 방향을 바꾸도록 하는 프로그램

---
+++ week12/week13_ch13_p393_car5.py
@@ -0,0 +1,37 @@
+"""
+p393 - 10개 객체를 생성하려면?
+리스트에 Car 객체를 10개 생성하여 저장하고, 각 자동차가 무작위로 전진하고 방향을 바꾸도록 하는 프로그램
+"""
+
+import random
+
+from turtle import *
+
+
+register_shape("car2.gif")
+
+
+class Car:
+    def __init__(self, speed, color, fname):
+        self.speed = speed
+        self.color = color
+        self.turtle = Turtle()
+        self.turtle.shape(fname)
+        self.turtle.speed(self.speed)
+
+    def drive(self, distance):
+        self.turtle.forward(distance)
+
+    def turnleft(self, degree):
+        self.turtle.left(degree)
+
+
+car_list = []  # 빈 리스트를 생성한다.
+for _ in range(10):
+    car_list.append(Car(random.randint(1, 10), "red", "car2.gif"))
+
+
+for _ in range(10):  # 10번 반복
+    for car in car_list:
+        car.drive(random.randint(50, 100))
+        car.turnleft(random.choice([0, 90, 180, 270]))
